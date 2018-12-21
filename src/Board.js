a9e9ae044aa21b570ebe7d459d79cdb030446b60
Kevin Yu <kevinyu05@gmail.com>
2018-12-21 12:09:24 -0800

    Added Score

--- src/Board.js
+++ src/Board.js
@@ -18,23 +18,30 @@ export default class Board extends Component {
       gameBoard: ["","","","","","","","",""],
       player1: [0,0,0,0,0,0,0,0,0],
       player2: [0,0,0,0,0,0,0,0,0],
+      pointerEvents: 'auto',
+      winningArray: [],
     }
   }
 
 
 createBoardLayout(){
-let freeze = null
 
-console.log("Sam",this.state.gameComplete);
-  if(this.state.gameComplete === false){
-    freeze = 'auto'
-  }else if (this.state.gameComplete === true){
-    freeze = 'none'
-  }
-    let arrayOfBoxes = this.state.gameBoard.map((value,i) => {return(
-      <Box id={i} key={i} attemptNumber={this.state.attempt}
-        getIndex={this.checkFunction.bind(this)} gameState={this.state.gameComplete}
-        letter={value} freezeBox={freeze}/> )})
+
+    let arrayOfBoxes = this.state.gameBoard.map((value,i) => {
+      if(value === ""){
+        return (
+          <Box id={i}  key={i} attemptNumber={this.state.attempt}
+            getIndex={this.checkFunction.bind(this)} gameState={this.state.gameComplete}
+            letter={value} pointerEvents={{pointerEvents: "auto"}} />
+        )
+      } else {
+        return(
+          <Box id={i}  key={i} attemptNumber={this.state.attempt}
+            getIndex={this.checkFunction.bind(this)} gameState={this.state.gameComplete}
+            letter={value} pointerEvents={{pointerEvents:"none"}}/>
+        )
+    }
+  })
 
     return arrayOfBoxes
   }
@@ -48,7 +55,12 @@ checkFunction(clickLocation){
 updateBoard(clickLocation){
   let {attempt} = this.state
   let gameBoard = [...this.state.gameBoard]
-  if(attempt % 2 === 0){
+  console.log("Game", this.state.gameComplete);
+  if(this.state.gameComplete === true){
+    console.log("Game Complete");
+    return {array:[], player:"Game Over"}
+  }
+  else if(attempt % 2 !== 0){
     let player2 = [...this.state.player2];
     player2.splice(clickLocation,1,1)
     gameBoard.splice(clickLocation,1,this.state.player2Icon)
@@ -64,15 +76,30 @@ updateBoard(clickLocation){
 }
 
 checkBoardandResult(playerObject){
-  let {gameComplete, winner, attempt} = this.state
+  let {gameComplete, winner, attempt, winningArray, player2Score, player1Score} = this.state
   var winningPossibilites = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],
                             [1,4,7],[2,5,8],[0,4,8],[2,4,6]]
 
 var resultsArray = winningPossibilites.filter((value) => this.testGame(value, playerObject.array))
 if(resultsArray.length > 0){
+  if(playerObject.player === "Player 1"){
+    let player1Score = this.state.player1Score
+    this.setState({player1Score: player1Score +1})
+  }else{
+    let player2Score = this.state.player2Score
+    this.setState({player2Score: player2Score +1})
+  }
+
+
+
+
   this.setState({gameComplete: true, winner: playerObject.player})
+
+
+
   for(let x of resultsArray[0]){
     document.getElementById(x).style.color="black"
+    this.setState({winningArray: resultsArray[0],  })
   }
 }else if(resultsArray.length === 0 && attempt === 8){
   this.setState({gameComplete: true, winner: "draw"})
@@ -97,34 +124,49 @@ listenForResult(){
   let {gameComplete, winner} = this.state
   if(gameComplete && winner === "draw"){
     return "The Game is a Draw!"
-  }else if(gameComplete){
+  }else if(gameComplete && winner === "Player 2"){
     return `${winner} is the winner!`
+  }else if(gameComplete && winner === "Player 1"){
+    return `${winner} is the winner!`
+}
+}
+
+resetGame(){
+  this.setState({
+    attempt: 0,
+    gameComplete: false,
+    winner: null,
+    gameBoard: ["","","","","","","","",""],
+    player1: [0,0,0,0,0,0,0,0,0],
+    player2: [0,0,0,0,0,0,0,0,0],
+    pointerEvents: 'auto',
+  })
+  if(this.state.winningArray.length > 0){
+  for(let value of this.state.winningArray){
+    document.getElementById(value).style.color=`#990000`
+  }
   }
 }
 
-reset(){
-  this.setState({
-    attempt:0,
-    gameComplete: false,
-    winner: null,
-    player1: [0,0,0,0,0,0,0,0,0],
-    player2: [0,0,0,0,0,0,0,0,0],
-  })
-}
-
 render(){
-console.log(this.state)
 this.listenForResult()
-
     return (
       <div>
+      <div className="ScoreBoard">
+      <h1>Score</h1>
+        <h2>Player 1 ({this.state.player1Score}) Player 2 ({this.state.player2Score})</h2>
+
+      </div>
       <div className="Results">
         <h1>{this.listenForResult()}</h1>
       </div>
       <div className="Game">
-        <div style={this.state.styles} className="board">
+        <div style={this.state.styles} className="board" >
           {this.createBoardLayout()}
         </div>
+      </div>
+      <div className='ResetGame'>
+        <h1 onClick={this.resetGame.bind(this)}>New Game</h1>
       </div>
       </div>
     )
--- src/boxes.js
+++ src/boxes.js
@@ -8,24 +8,11 @@ export default class Box extends Component {
     super(props)
     this.letter = ""
     this.state = {
-      pointerEvents: 'auto',
+      
     }
   }
 
-updateBox(event){
-  let {pointerEvents} = this.state
-  console.log(this.props.gameState);
-  if(this.props.gameState){
-    return "complete"
-  } else if(this.props.attemptNumber%2 === 0){
-    this.letter='X'
-    this.forceUpdate()
-  } else {
-    this.letter='0'
-    this.forceUpdate()
-  }
-    this.setState({pointerEvents: 'none'})
-}
+
 
 handleIndexChange(event){
     this.props.getIndex(event.target.id)
@@ -33,13 +20,13 @@ handleIndexChange(event){
 
   onClickFunction(event){
     this.handleIndexChange(event)
-    this.updateBox(event.target.id)
   }
 
+
   render(){
-    console.log("props",this.props);
+
     return (
-        <div className="box" style={this.state} id={this.props.id}  onClick={this.onClickFunction.bind(this)}>
+        <div className="box" style={this.props.pointerEvents} id={this.props.id}  onClick={this.onClickFunction.bind(this)}>
           {this.props.letter}
         </div>
     )
